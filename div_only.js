be5513ca43cd387c7e6dcb3f2a68219add416911
JoostJacob <JoostJacob@users.noreply.github.com>
2025-02-03 23:18:49 +0100

    Update div_only.js
    Function concatenation

--- div_only.js
+++ div_only.js
@@ -1,4 +1,4 @@
-
-var result = 40 + 2;
-document.getElementById("resultDiv").innerHTML=result;
+function add1(x) { return x+1; }
+function times10(x) { return x*10; }
+document.getElementById("resultDiv").innerHTML=add1(add1(times10(4)));
 
