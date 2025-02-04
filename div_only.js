aed9a38e6f79afecf4a8ff5208508d977ce46e7d
JoostJacob <JoostJacob@users.noreply.github.com>
2025-02-04 11:33:18 +0100

    Update div_only.js
    back to 42

--- div_only.js
+++ div_only.js
@@ -1,4 +1,4 @@
 function add1(x) { return x+1; }
 function times10(x) { return x*10; }
-document.getElementById("resultDiv").innerHTML=add1(add1(add1(times10(4))));
+document.getElementById("resultDiv").innerHTML=add1(add1(times10(4)));
 
