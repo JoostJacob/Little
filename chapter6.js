90c7d17dbd0db7e0ceb6a3c1af2a9a90ff264979
JoostJacob <JoostJacob@users.noreply.github.com>
2025-03-06 13:20:42 +0100

    Create chapter6.js

---
+++ chapter6.js
@@ -0,0 +1,26 @@
+
+function isNumbered(s) {
+  return isAtom(s)
+  ? isNumber(s)
+  : isNull(cdr(s))
+  ? isNumber(car(s))
+  : isNumber(car(s)) && (isEq(car(cdr(s)), "+") || isEq(car(cdr(s)), "*") || 
+                         isEq(car(cdr(s)), "^") || isEq(car(cdr(s)), "%")) && isNumbered(car(cdr(cdr(s))));
+}
+
+function value(s) {
+  function p3(s) {
+    return car(cdr(cdr(s)));
+  }
+  return isAtom(s)
+  ? s
+  : isEq(car(cdr(s)), "+")
+  ? plus(value(car(s)), value(p3(s)))
+  : isEq(car(cdr(s)), "-")
+  ? minus(value(car(s)), value(p3(s)))
+  : isEq(car(cdr(s)), "*")
+  ? star(value(car(s)), value(p3(s)))
+  : isEq(car(cdr(s)), "%")
+  ? quotient(value(car(s)), value(p3(s)))
+  : "error";
+}
